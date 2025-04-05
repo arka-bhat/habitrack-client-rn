638981cbee93ecaaad7f57a288be84ee9902dc00
arka-bhat <arka.bhattacharya33@gmail.com>
2025-04-05 15:50:52 +0530

    update assets layout

--- app/assets/_layout.tsx
+++ app/assets/_layout.tsx
@@ -4,8 +4,9 @@ import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 const Layout = () => {
     return (
         <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name='addOrUpdateAsset' />
-            <Stack.Screen name='deleteAsset' />
+            <Stack.Screen name='[id]' />
+            <Stack.Screen name='addOrUpdate' />
+            <Stack.Screen name='delete' />
         </Stack>
     );
 };
